7c6c3227a9828969ddc3997eae66efa1c695f8b6
cpmachado <cpmachado@protonmail.com>
2026-03-21 22:33:30 +0000

    sunnywarm

---
+++ the-complete-cpp-developer-course/02-fundamentals/sunnywarm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main() {
+    bool isSunny = false;
+    bool isWarm = true;
+
+    cout << boolalpha;
+    cout << "isSunny = " << isSunny << endl;
+    cout << "isWarm  = " << isWarm << endl;
+    return 0;
+}
