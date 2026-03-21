fafe111f956e96c15ca62b251d932f0f9a239ab4
cpmachado <cpmachado@protonmail.com>
2026-03-21 22:25:40 +0000

    textvariables

---
+++ the-complete-cpp-developer-course/02-fundamentals/textvariables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int
+main() {
+    char singleChar = 'a';
+    string myName = "cpmachado";
+
+    cout << "singleChar = " << singleChar << endl;
+    cout << "myName     = " << myName << endl;
+
+    cout << "hello " << myName << endl;
+    return 0;
+}
